8e8a4865e3375756e99dccb282770cc44da0d0d3
Max <linme00p@gmail.com>
2024-06-10 18:08:17 -0400

    move method display from Grep. move methods for match finding in Grep.

--- src/lib.rs
+++ src/lib.rs
@@ -35,68 +35,60 @@ impl Grep {
 
     pub fn run(&self) -> io::Result<()> {
         if self.files.is_empty() {
-            return Ok(self.process_stdin()?);
+            let matches = self.process_stdin()?;
+            display_matches(false, matches, &PathBuf::from("stdin"));
         }
         for file_path in &self.files {
             let matches = self.process_file(file_path)?;
-            self.display_matches(matches, file_path);
+            display_matches(self.files.len() > 1,matches, file_path);
         }
         Ok(())
     }
 
-    fn process_stdin(&self) -> io::Result<()> {
+    fn process_stdin(&self) -> io::Result<Vec<Match>> {
         let stdin = io::stdin();
         let reader = stdin.lock();
         let matches = if self.is_expression {
-            find_regex_matches(&self.pattern, reader)?
+            self.find_regex_matches(reader)?
         } else {
-            find_exact_matches(&self.pattern, reader)?
+            self.find_exact_matches(reader)?
         };
-        self.display_matches(matches, &PathBuf::from("stdin"));
-        Ok(())
+        Ok(matches)
     }
 
     fn process_file(&self, file_path: &PathBuf) -> io::Result<Vec<Match>> {
         let file = std::fs::File::open(file_path)?;
         let reader = std::io::BufReader::new(file);
         if self.is_expression {
-            find_regex_matches(&self.pattern, reader)
+            self.find_regex_matches(reader)
         } else {
-            find_exact_matches(&self.pattern, reader)
+            self.find_exact_matches(reader)
         }
     }
 
-    fn display_matches(&self, matches: Vec<Match>, file_path: &PathBuf) {
-        for match_line in matches {
-            if self.files.len() > 1 {
-                print!("{}:", file_path.display());
+    fn find_exact_matches<R: BufRead>(&self, buf_reader: R) -> io::Result<Vec<Match>> {
+        let mut results = Vec::new();
+        for line in buf_reader.lines() {
+            let line = line?;
+            if line.contains(self.pattern.as_str()) {
+                results.push(colorize_match(&line, self.pattern.as_str()));
             }
-            print_match(&match_line);
         }
+        Ok(results)
     }
-}
 
-fn find_exact_matches<R: BufRead>(pattern: &str, buf_reader: R) -> io::Result<Vec<Match>> {
-    let mut results = Vec::new();
-    for line in buf_reader.lines() {
-        let line = line?;
-        if line.contains(pattern) {
-            results.push(colorize_match(&line, pattern));
+    fn find_regex_matches<R: BufRead>(&self, buf_reader: R) -> io::Result<Vec<Match>> {
+        let re = Regex::new(self.pattern.as_str()).unwrap();
+        let mut results = Vec::new();
+        for line in buf_reader.lines() {
+            let line = line?;
+            for mat in re.find_iter(&line) {
+                results.push(colorize_match(&line, mat.as_str()));
+            }
         }
+        Ok(results)
     }
-    Ok(results)
-}
 
-fn find_regex_matches<R: BufRead>(pattern: &str, buf_reader: R) -> io::Result<Vec<Match>> {
-    let re = Regex::new(pattern).unwrap();
-    let mut results = Vec::new();
-    for line in buf_reader.lines() {
-        let line = line?;
-        for mat in re.find_iter(&line) {
-            results.push(colorize_match(&line, mat.as_str()));
-        }
-    }
-    Ok(results)
 }
 
 fn colorize_match(text: &str, pattern: &str) -> Match {
@@ -116,4 +108,13 @@ fn print_match(match_line: &Match) {
         print!("{}", part);
     }
     println!();
+}
+
+fn display_matches(is_multiple_files: bool, matches: Vec<Match>, file_path: &PathBuf) {
+    for match_line in matches {
+        if is_multiple_files {
+            print!("{}:", file_path.display());
+        }
+        print_match(&match_line);
+    }
 }
